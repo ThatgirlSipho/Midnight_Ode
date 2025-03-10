610f0d9c06c10a72a47efdb0d6ccdbe9d93da429
ThatgirlSipho <mkhefasipho@gmail.com>
2025-03-11 01:00:37 +0200

    Just created all basic components to be used

---
+++ src/app/subscribtion-form/subscribtion-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribtion-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './subscribtion-form.component.html',
+  styleUrl: './subscribtion-form.component.css'
+})
+export class SubscribtionFormComponent {
+
+}
